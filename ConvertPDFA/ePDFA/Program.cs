66266d9faa65c7bff20e93cc251e973dfdff5101
Wanderson Caldas <wcaldasti@gmail.com>
2019-08-13 21:26:37 -0300

    CRIAR PROJETO

---
+++ ConvertPDFA/ePDFA/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using SautinSoft.Document;
+
+namespace ePDFA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoadAndSaveAsPDFA();
+        }
+
+        public static void LoadAndSaveAsPDFA()
+        {            
+            string loadPath = @"C:\Users\wanderson\Desktop\Laura\teste.pdf";            
+            DocumentCore document = DocumentCore.Load(loadPath);
+            PdfSaveOptions options = new PdfSaveOptions()
+            {                
+                Compliance = PdfCompliance.PDF_A
+            };
+            string savePath = Path.ChangeExtension(loadPath, ".pdf");
+            document.Save(savePath, options);            
+            System.Diagnostics.Process.Start(savePath);
+        }
+    }
+}
